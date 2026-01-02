1b16175e1a11800b3581af8368f2f868d24b2857
Dheeraj Basude <dheerajbe123@gmail.com>
2026-01-02 22:25:48 +0530

    feat: added first

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const usersRouter = require('./routes/users');
+const productsRouter = require('./routes/products');
+
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+
+// Use the routers
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
+
+app.get('/', (req, res) => {
+  res.send('Main application home page');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
